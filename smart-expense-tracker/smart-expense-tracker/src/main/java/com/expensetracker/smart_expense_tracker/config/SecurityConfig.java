569c1771513cf7fdb752f7d1b250e17aae3965af
Sreesanth-01 <iamshanth7@gmail.com>
2026-01-06 14:34:01 +0530

    feat: jwt secured endpoint

--- smart-expense-tracker/smart-expense-tracker/src/main/java/com/expensetracker/smart_expense_tracker/config/SecurityConfig.java
+++ smart-expense-tracker/smart-expense-tracker/src/main/java/com/expensetracker/smart_expense_tracker/config/SecurityConfig.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.expensetracker.smart_expense_tracker.security.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,7 +25,8 @@ public class SecurityConfig {
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/health/**").permitAll()
                 .anyRequest().authenticated()
-            );
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
---
+++ smart-expense-tracker/smart-expense-tracker/src/main/java/com/expensetracker/smart_expense_tracker/security/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.expensetracker.smart_expense_tracker.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,FilterChain filterChain) throws ServletException,IOException{
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader!=null && authHeader.startsWith("Bearer "))
+        {
+            String token = authHeader.substring(7);
+
+            if(jwtUtil.validateToken(token)){
+                String email = jwtUtil.extractEmail(token);
+                
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email,null,null); //email,password(already authenticated),role(later)
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); //Not mandatory but good practice.(sets Ip address and other details if needed)
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
+
+
+        
