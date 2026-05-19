a8422622ea35a29eb67ff83cb3d3091f1a0b9e21
Sreesanth-01 <iamshanth7@gmail.com>
2026-05-20 00:28:20 +0530

    deployment: Updated cors

--- smart-expense-tracker/smart-expense-tracker/src/main/java/com/expensetracker/smart_expense_tracker/config/SecurityConfig.java
+++ smart-expense-tracker/smart-expense-tracker/src/main/java/com/expensetracker/smart_expense_tracker/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {  //for allowing frontend integration
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","flux-expense-tracker-livid.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
